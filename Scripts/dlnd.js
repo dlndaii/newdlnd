8b74d609334afd7cd2fd0e012885e231176f5007
dlndaii <daidinh9874@gmail.com>
2025-08-21 13:27:30 +0700

    Update dlnd.js

--- Scripts/dlnd.js
+++ Scripts/dlnd.js
@@ -8,7 +8,7 @@ var ua = $request.headers["User-Agent"] || $request.headers["user-agent"],
 
 obj.Attention = "Chúc mừng bạn! Vui lòng không bán hoặc chia sẻ cho người khác!";
 
-var locket02 = {
+var dlnd = {
   is_sandbox: !1,
   ownership_type: "PURCHASED",
   billing_issues_detected_at: null,
@@ -16,15 +16,15 @@ var locket02 = {
   expires_date: "2099-12-18T01:04:17Z",
   grace_period_expires_date: null,
   unsubscribe_detected_at: null,
-  original_purchase_date: "1997-02-11T00:00:00Z",
-  purchase_date: "1997-02-11T00:00:00Z",
+  original_purchase_date: "2025-08-16T00:00:00Z",
+  purchase_date: "2025-08-16T00:00:00Z",
   store: "app_store"
 };
 
 var locket01 = {
   grace_period_expires_date: null,
-  purchase_date: "1997-02-11T00:00:00Z",
-  product_identifier: "com.locket02.premium.yearly",
+  purchase_date: "2025-08-16T00:00:00Z",
+  product_identifier: "com.dlnd.premium.yearly",
   expires_date: "2099-12-18T01:04:17Z"
 };
 
@@ -34,12 +34,13 @@ if (match) {
   let [e, s] = mapping[match];
   s
     ? (locket01.product_identifier = s,
-       obj.subscriber.subscriptions[s] = locket02)
-    : obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+       obj.subscriber.subscriptions[s] = dlnd)
+    : obj.subscriber.subscriptions["com.dlnd.premium.yearly"] = dlnd;
   obj.subscriber.entitlements[e] = locket01;
 } else {
-  obj.subscriber.subscriptions["com.locket02.premium.yearly"] = locket02;
+  obj.subscriber.subscriptions["com.dlnd.premium.yearly"] = dlnd;
   obj.subscriber.entitlements.pro = locket01;
 }
+
 
 $done({ body: JSON.stringify(obj) });
